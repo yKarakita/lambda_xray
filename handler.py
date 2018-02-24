0b11f8aa52433ce591d7294b4b363a552767f364
yKarakita <taf.749@gmail.com>
2018-02-24 11:32:20 +0900

    [add] subsegment metadata.

--- handler.py
+++ handler.py
@@ -1,10 +1,11 @@
 import json
+import uuid
+
 from aws_xray_sdk.core import xray_recorder
 
 
-@xray_recorder.capture('response hello decorator')
+@xray_recorder.capture('response hello')
 def hello(event, context):
-    # subsegment = xray_recorder.begin_subsegment('subsegment_name')
 
     body = {
         "message": "Hello!",
@@ -17,9 +18,10 @@ def hello(event, context):
         "body": json.dumps(body)
     }
 
-    # subsegment.put_metadata('key', dict, 'namespace')
-    # subsegment.put_annotation('response_status_code', status_code)
     subsegment = xray_recorder.current_subsegment()
     subsegment.put_annotation('response_status_code', status_code)
+
+    subsegment.put_metadata('key01', {'id': str(uuid.uuid4())}, 'namespace01')
+    subsegment.put_metadata('key02', {'id': str(uuid.uuid4())}, 'namespace01')
 
     return response
