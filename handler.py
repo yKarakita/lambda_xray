86017dc1c23a0309e675cfb60b37934f1f7c84d6
yKarakita <taf.749@gmail.com>
2018-02-24 09:17:09 +0900

    [update] Use aws-xray in handler.

--- handler.py
+++ handler.py
@@ -1,7 +1,11 @@
 import json
+from aws_xray_sdk.core import xray_recorder
 
 
 def hello(event, context):
+    # subsegment = xray_recorder.begin_subsegment('subsegment_name')
+    xray_recorder.begin_subsegment('response hello')
+
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
@@ -11,5 +15,10 @@ def hello(event, context):
         "statusCode": 200,
         "body": json.dumps(body)
     }
+
+    # subsegment.put_metadata('key', dict, 'namespace')
+    # subsegment.put_annotation('key', 'value')
+
+    xray_recorder.end_subsegment()
 
     return response
