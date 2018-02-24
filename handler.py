b792245551b10f1ab43d84c1aae2d945da61feab
yKarakita <taf.749@gmail.com>
2018-02-24 12:28:37 +0900

    [add] put s3 function.

--- handler.py
+++ handler.py
@@ -1,27 +1,34 @@
 import json
 import uuid
-
+import boto3
+import os
 from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch
+
+patch(['boto3'])
+
+s3_client = boto3.client('s3')
+
+bucket_name = os.environ['S3_BUCKET']
 
 
 @xray_recorder.capture('response hello')
 def hello(event, context):
+    body = event['queryStringParameters']['keyword']
 
-    body = {
-        "message": "Hello!",
-    }
-
-    status_code = 200
+    put_object_into_s3(bucket_name, str(uuid.uuid4()), body)
 
     response = {
-        "statusCode": status_code,
-        "body": json.dumps(body)
+        "statusCode": 200,
+        "body": json.dumps({'message': 'ok'})
     }
 
-    subsegment = xray_recorder.current_subsegment()
-    subsegment.put_annotation('response_status_code', status_code)
-
-    subsegment.put_metadata('key01', {'id': str(uuid.uuid4())}, 'namespace01')
-    subsegment.put_metadata('key02', {'id': str(uuid.uuid4())}, 'namespace01')
-
     return response
+
+
+@xray_recorder.capture('put_object')
+def put_object_into_s3(bucket_name, bucket_key, body):
+    subsegment = xray_recorder.current_subsegment()
+    response = s3_client.put_object(Bucket=bucket_name, Key=bucket_key, Body=body)
+    status_code = response['ResponseMetadata']['HTTPStatusCode']
+    subsegment.put_annotation('put_response', status_code)
